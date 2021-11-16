e6768784f0c2872d72db9db7c723f9f65963fdff
Christine Yoo <christinecho.s@gmail.com>
2021-11-16 11:32:54 -0800

    Included date picker

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { useColorMode } from 'theme-ui';
 import { useMediaQuery } from 'react-responsive';
 import { Route, Routes } from 'react-router-dom';
 import About from './About';
+import { MuiPickersUtilsProvider, DatePicker } from '@material-ui/pickers';
+import { TextField } from '@material-ui/core';
+import DateFnsUtils from '@date-io/date-fns';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
   const [colorMode, setColorMode] = useColorMode();
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   {/* useMediaQuery({
     query: '(prefers-color-scheme: dark)', //this is a boolean
@@ -16,18 +21,29 @@ function App() {
   
 
   return (
-    <>
-      <header className="App-header">
+      <>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <header className="App-header">
         <h1>
           Theme UI Playground
         </h1>
         <button onClick={() => setColorMode(colorMode === 'light' ? 'dark' : 'light')} sx={{bg: 'primary', color: 'inverseText'}}>Toggle {colorMode === 'light' ? 'Dark' : 'Light'}</button>
+        <DatePicker format='MM/dd/yyyy' id='date-picker' label='Date Of Birth' value={selectedDate} onChange={date => setSelectedDate(date)}  />
+        <DatePicker 
+        renderInput={props => <TextField {...props} />}
+        value={selectedDate}
+        label='Second Thing'
+        onChange={date => setSelectedDate(date)}
+      />
       </header>
       <Routes>
           <Route path='/about' element={<About />} />
       </Routes>
-    </>
+      
     
+      
+    </MuiPickersUtilsProvider>
+    </>
   );
 }
 
